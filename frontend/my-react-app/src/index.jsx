ca39f120f293081d8741d2a7d6df85883ce62dd7
Shreeya Tuladhar <tuladhars982@gmail.com>
2025-03-26 16:57:32 +1100

    Add NavBar

--- frontend/my-react-app/src/index.jsx
+++ frontend/my-react-app/src/index.jsx
@@ -1,17 +1,4 @@
 import { StrictMode } from 'react'
-<<<<<<< HEAD
-import { createRoot }  from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
-=======
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
@@ -20,4 +7,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
->>>>>>> bcde91807c66792c1dd490dcc3451b050c31c3c3
---
+++ frontend/my-react-app/src/pages/dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+function Dashboard() {
+    return(
+        <div>
+            {<NavBar />}
+            <h1>Welcome to your Dashboard!</h1>
+        </div>
+        
+    );
+
+}
+
+export default Dashboard;
