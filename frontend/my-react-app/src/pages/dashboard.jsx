6fd4bb0c24803da188e1cde7477f8083cfd68a82
Shreeya Tuladhar <tuladhars982@gmail.com>
2025-04-01 12:25:37 +1100

    Separate login inputs

--- frontend/my-react-app/src/pages/dashboard.jsx
+++ frontend/my-react-app/src/pages/dashboard.jsx
@@ -1,12 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import "../styles/dashboard.css";
-
-function Dashboard(){
-    return(
-        <h4>Welcome to your dashboard!</h4>
-    );
-=======
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 import Calender from "./calender";
@@ -27,7 +19,6 @@ function Dashboard() {
         
     );
 
->>>>>>> b480e58f84d528de73b4c5fad28fa2b7b5ed8e37
 }
 
 export default Dashboard;
--- frontend/my-react-app/src/pages/login.jsx
+++ frontend/my-react-app/src/pages/login.jsx
@@ -1,52 +1,33 @@
 import React from "react";
-<<<<<<< HEAD
-import {useNavigate} from "react-router-dom";
 import "../styles/login.css";
-=======
-import '../styles/login.css';
 import { useNavigate } from "react-router-dom";
->>>>>>> b480e58f84d528de73b4c5fad28fa2b7b5ed8e37
+import Input from "../components/input";
 
 function Login() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  function handleLogin() {
+    navigate("/dashboard");
+  }
 
-    function handleLogin(){
-        navigate("/dashboard");
-<<<<<<< HEAD
-    };
+  return (
+    <div className="login-container">
+      <h1>Wellness Centre Bookings</h1>
+      <div className="login-box">
+        <form onSubmit={handleLogin}>
+          {<Input type="text" name="username" placeholder="Username" />}
+          {<Input type="password" name="password" placeholder="Password" />}
 
-    return(
-        <div class="login-container">
-                <h1>Wellness Centre Bookings</h1>
-                <div class="login-box">
-                  <form action={handleLogin} method="POST">
-                    <input type="text" className="inputs" name="username" placeholder="Username" />
-                    <input type="text" className="inputs" name="password" placeholder="Password" />
-                    <button type="submit" class="btn btn-success btn-lg" value="login">Login</button>
-                  </form>               
-                </div>
-            </div>  
-    );
+          <button
+            type="submit"
+            className="btn btn-success btn-lg"
+            value="login">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-=======
-    }
-
-    return(
-        <div className="login-container">
-            <h1>Wellness Centre Bookings</h1>
-            <div className="login-box">
-              <form onSubmit={handleLogin}>
-                <input type="text" className="inputs" name="username" placeholder="Username" />
-                <input type="text" className="inputs" name="password" placeholder="Password" />
-                <button type="submit" className="btn btn-success btn-lg" value="login">Login</button>
-              </form>               
-            </div>
-        </div> 
-    )
-
-};
->>>>>>> b480e58f84d528de73b4c5fad28fa2b7b5ed8e37
 
 export default Login;
